2b06213ca2f7e482dea86b571e4abf78f4a32e45
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-06-20 13:35:30 -0400

    Update GeracaoQRCodeService.java

--- casos-de-teste/src/GeracaoQRCodeService.java
+++ casos-de-teste/src/GeracaoQRCodeService.java
@@ -1,1 +1,32 @@
+public class GeracaoQRCodeService {
 
+    public String validarGeracaoQRCode(
+            boolean tecladoNumerico,
+            boolean qrDinamico,
+            boolean exibicaoCorreta,
+            boolean chavePixCorreta,
+            boolean copiaColaFunciona,
+            boolean notificacaoRecebimento
+    ) {
+
+        if (!tecladoNumerico)
+            return "ERRO_ENTRADA_VALOR";
+
+        if (!qrDinamico)
+            return "QR_INVALIDO";
+
+        if (!exibicaoCorreta)
+            return "ERRO_EXIBICAO";
+
+        if (!chavePixCorreta)
+            return "CHAVE_PIX_INVALIDA";
+
+        if (!copiaColaFunciona)
+            return "ERRO_COPIA_COLA";
+
+        if (!notificacaoRecebimento)
+            return "NOTIFICACAO_AUSENTE";
+
+        return "SUCESSO";
+    }
+}
