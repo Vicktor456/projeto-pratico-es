0c65b2dfe2696b40bce431c5d4645cbbd7678102
oTarakna <164069689+oTarakna@users.noreply.github.com>
2026-06-20 13:37:38 -0400

    Update AlertaFraudeService.java

--- casos-de-teste/tests/AlertaFraudeService.java
+++ casos-de-teste/tests/AlertaFraudeService.java
@@ -1,37 +1,161 @@
-public class AlertaFraudeService {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    public String validarAlertaFraude(
-            boolean chaveSuspeita,
-            boolean chaveCritica,
-            boolean layoutCorreto,
-            boolean linguagemAdequada,
-            boolean cancelamentoFunciona,
-            boolean cronometroExecutado,
-            boolean bloqueioCriticoFunciona
-    ) {
+public class AlertaFraudeServiceTest {
 
-        if (chaveCritica) {
-            if (!bloqueioCriticoFunciona)
-                return "FALHA_BLOQUEIO_CRITICO";
+    private final AlertaFraudeService service =
+            new AlertaFraudeService();
 
-            return "BLOQUEIO_REALIZADO";
-        }
+    @Test
+    void CT_US14_01_ContinuarComCronometro() {
 
-        if (!chaveSuspeita)
-            return "ALERTA_INDEVIDO";
+        assertEquals(
+                "SUCESSO",
+                service.validarAlertaFraude(
+                        true,
+                        false,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+    }
 
-        if (!layoutCorreto)
-            return "ERRO_LAYOUT";
+    @Test
+    void CT_US14_02_CancelamentoSeguro() {
 
-        if (!linguagemAdequada)
-            return "LINGUAGEM_INADEQUADA";
+        assertEquals(
+                "SUCESSO",
+                service.validarAlertaFraude(
+                        true,
+                        false,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+    }
 
-        if (!cancelamentoFunciona)
-            return "CANCELAMENTO_IGNORADO";
+    @Test
+    void CT_US14_03_BloqueioDeRiscoCritico() {
 
-        if (!cronometroExecutado)
-            return "CRONOMETRO_NAO_EXECUTADO";
+        assertEquals(
+                "BLOQUEIO_REALIZADO",
+                service.validarAlertaFraude(
+                        false,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+    }
 
-        return "SUCESSO";
+    @Test
+    void CT_US14_04_AlertaExibidoParaChaveSegura() {
+
+        assertEquals(
+                "ALERTA_INDEVIDO",
+                service.validarAlertaFraude(
+                        false,
+                        false,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+    }
+
+    @Test
+    void CT_US14_05_FalhaDeLayout() {
+
+        assertEquals(
+                "ERRO_LAYOUT",
+                service.validarAlertaFraude(
+                        true,
+                        false,
+                        false,
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+    }
+
+    @Test
+    void CT_US14_06_LinguagemInadequada() {
+
+        assertEquals(
+                "LINGUAGEM_INADEQUADA",
+                service.validarAlertaFraude(
+                        true,
+                        false,
+                        true,
+                        false,
+                        true,
+                        true,
+                        true
+                )
+        );
+    }
+
+    @Test
+    void CT_US14_07_CancelamentoIgnorado() {
+
+        assertEquals(
+                "CANCELAMENTO_IGNORADO",
+                service.validarAlertaFraude(
+                        true,
+                        false,
+                        true,
+                        true,
+                        false,
+                        true,
+                        true
+                )
+        );
+    }
+
+    @Test
+    void CT_US14_08_CronometroNaoExecutado() {
+
+        assertEquals(
+                "CRONOMETRO_NAO_EXECUTADO",
+                service.validarAlertaFraude(
+                        true,
+                        false,
+                        true,
+                        true,
+                        true,
+                        false,
+                        true
+                )
+        );
+    }
+
+    @Test
+    void CT_US14_09_FalhaBloqueioCritico() {
+
+        assertEquals(
+                "FALHA_BLOQUEIO_CRITICO",
+                service.validarAlertaFraude(
+                        false,
+                        true,
+                        true,
+                        true,
+                        true,
+                        true,
+                        false
+                )
+        );
     }
 }
